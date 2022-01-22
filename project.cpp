fc38ae5c430bab361a6391ee86fa0803c308dabe
Rómulo Kaidussis <rkaidussis@gmail.com>
2022-01-22 17:18:24 +0000

    same shit, differet approach

--- project.cpp
+++ project.cpp
@@ -9,42 +9,58 @@ struct node{
     char colour = 'w';
 };
 
-vector<int> find_all_parents(vector<node> nodes, vector<int> vertices, int vertex){
-    if(nodes[vertex-1].parents.empty())
+vector<node> nodes;
+
+vector<int> LCA(vector<int> vertices, int vertex){
+    cout << "VERTEX: " << vertex << " COLOUR: " << nodes[vertex-1].colour << endl;
+    if(nodes[vertex-1].parents.empty()){
+        if(nodes[vertex-1].colour == 'r'){
+            nodes[vertex-1].colour = 'y';
+            vertices.push_back(vertex);
+            }
         return vertices;
+        }
     for(auto iter: nodes[vertex-1].parents){
-        vertices.push_back(iter);
-        
-        vertices = find_all_parents(nodes, vertices, iter);
+        if(nodes[iter-1].colour == 'r'){
+            nodes[iter-1].colour = 'y';
+            vertices.push_back(iter);
+            }
+        vertices = LCA(vertices, iter);
     }
     return vertices;
 }
 
-int is_parent(vector<node> nodes, int u, int v){
-    for(auto iter: nodes[u-1].parents)
-        if(iter == v)
-            return 0;
-    return -1;
+vector<int> u_parents(vector<int> vertices, int vertex){
+    if(nodes[vertex-1].parents.empty()){
+        return vertices;
+        }
+    for(auto iter: nodes[vertex-1].parents){
+        nodes[iter-1].colour = 'r';
+        vertices.push_back(iter);
+        vertices = u_parents(vertices, iter);
+    }
+    return vertices;
 }
 
-void LCA(vector<node> nodes, int u, int v){
-    vector<int> aux1 = {u};
-    vector<int> aux2 = {v};
-    vector<int> result;
-    aux1 = find_all_parents(nodes, aux1, u);
-    aux2 = find_all_parents(nodes, aux2, v);
-    
-    for (auto i: aux2){
-        for(auto e: aux1)
-            if(i == e)
-                result.push_back(i);
-    }
+bool is_parent(int child, int parent){
+    for(auto iter: nodes[child-1].parents)  
+        if(iter == parent)
+            return true;
+    return false;
+}
+
+void pre_processing(int u, int v){
+    vector<int> aux1;
+    vector<int> aux2;
+    nodes[u-1].colour = 'r';
+    aux1 = u_parents(aux1, u);
+    aux2 = LCA(aux2, v);
     aux1.clear();
-    aux1.push_back(result[0]);
-    int size = (int)result.size();
-    for(int i=1; i < size; i++)
-        if(is_parent(nodes, result[i-1], result[i]) == -1)
-            aux1.push_back(result[i]);
+    for(auto parent: aux2)
+        for(auto child: aux2)
+            if(child!=parent && !is_parent(child, parent))
+                aux1.push_back(parent);
+            
 
     for(auto iter: aux1)
         cout << iter << ' ';
@@ -52,12 +68,12 @@ void LCA(vector<node> nodes, int u, int v){
 }
 
 
-int dfs_visit(vector<node> nodes, int vertex){ 
+int dfs_visit(int vertex){ 
     
     nodes[vertex].colour = 'g';
     for (auto iter: nodes[vertex].parents){
         if(nodes[iter-1].colour == 'w'){
-            if(dfs_visit(nodes, iter-1) == -1)
+            if(dfs_visit(iter-1) == -1)
                 return -1;
             }
         else if(nodes[iter-1].colour == 'g'){
@@ -70,10 +86,10 @@ int dfs_visit(vector<node> nodes, int vertex){
     return 0;
 }
 
-void dfs(vector<node> nodes, int size){
+void dfs(int size){
     for (int i=0; i < size; i++){
         if(nodes[i].colour == 'w'){
-            if (dfs_visit(nodes, i) == -1){
+            if (dfs_visit(i) == -1){
                 break;
             }
         }
@@ -87,7 +103,9 @@ int main()
         return -1;
     if(scanf("%d %d", &vertices, &edges)!=2)
         return -1;
-    vector<node> nodes(vertices);
+    node vertex;
+    for(int i = 0; i < vertices; i++)
+        nodes.push_back(vertex);
     int i = 0;
     while(i<edges){
         if(scanf("%d %d", &parent, &child)!=2)
@@ -105,7 +123,7 @@ int main()
         cout << "-" << endl;
         return 0;
     }    
-    dfs(nodes, vertices);
-    LCA(nodes, u, v);
+    dfs(vertices);
+    pre_processing(u, v);
     return 0;
 }
