e81b6ccc42bba0eda8cb65d102d81e907d2c083b
Rómulo Kaidussis <rkaidussis@gmail.com>
2022-01-22 18:51:12 +0000

    same shit, same approach, new fail

--- project.cpp
+++ project.cpp
@@ -12,7 +12,6 @@ struct node{
 vector<node> nodes;
 
 vector<int> LCA(vector<int> vertices, int vertex){
-    cout << "VERTEX: " << vertex << " COLOUR: " << nodes[vertex-1].colour << endl;
     if(nodes[vertex-1].parents.empty()){
         if(nodes[vertex-1].colour == 'r'){
             nodes[vertex-1].colour = 'y';
@@ -42,29 +41,38 @@ vector<int> u_parents(vector<int> vertices, int vertex){
     return vertices;
 }
 
+
+
 bool is_parent(int child, int parent){
-    for(auto iter: nodes[child-1].parents)  
-        if(iter == parent)
-            return true;
+    cout << "CHILD: " << child << " PARENT: " << parent;
+    if(nodes[child-1].parents.empty())
+        return false;
+    for(auto iter: nodes[child-1].parents){  
+        if(iter != parent){
+            cout << "FALSE" << endl;
+            if(is_parent(parent, iter) == false)
+                return false;
+            }
+        if( iter == parent)
+        {cout << "TRUE" << endl;
+        return true;}
+    }
     return false;
 }
 
-void pre_processing(int u, int v){
+vector<int> pre_processing(int u, int v){
     vector<int> aux1;
     vector<int> aux2;
     nodes[u-1].colour = 'r';
     aux1 = u_parents(aux1, u);
     aux2 = LCA(aux2, v);
     aux1.clear();
-    for(auto parent: aux2)
-        for(auto child: aux2)
-            if(child!=parent && !is_parent(child, parent))
-                aux1.push_back(parent);
-            
-
-    for(auto iter: aux1)
-        cout << iter << ' ';
-    cout << endl;
+    aux1.push_back(aux2[0]);
+    for (int i = 1; i < (int)aux2.size(); i++)
+        if (!is_parent(aux2[i-1], aux2[i]))
+            aux1.push_back(aux2[i]);
+    sort(aux1.begin(), aux1.end());
+    return aux1;
 }
 
 
@@ -77,7 +85,6 @@ int dfs_visit(int vertex){
                 return -1;
             }
         else if(nodes[iter-1].colour == 'g'){
-            cout << "0" << endl;
             return -1;
         }
     }
@@ -86,19 +93,21 @@ int dfs_visit(int vertex){
     return 0;
 }
 
-void dfs(int size){
+int dfs(int size){
     for (int i=0; i < size; i++){
         if(nodes[i].colour == 'w'){
             if (dfs_visit(i) == -1){
-                break;
+                return -1;
             }
         }
     }
+    return 0;
 }
 
 int main()
 {
     int u, v, vertices, edges, parent, child;
+    vector<int> res;
     if(scanf("%d %d", &u, &v)!=2)
         return -1;
     if(scanf("%d %d", &vertices, &edges)!=2)
@@ -112,18 +121,37 @@ int main()
             return -1;
         
         if(nodes[child-1].parents.size() == 2){
-            printf("HERE 2");
             cout << "0" << endl;
             return 0;
         }
         nodes[child-1].parents.push_back(parent);
         i++;
     }
+    if(dfs(vertices) == -1){
+        cout << "0" << endl;
+        return 0;}
+
     if(nodes[u-1].parents.empty() && nodes[v-1].parents.empty()){
         cout << "-" << endl;
         return 0;
-    }    
-    dfs(vertices);
-    pre_processing(u, v);
+    }
+    
+    if(is_parent(u, v)){
+        cout << "HERE";
+        cout << v;
+        return 0;}
+        
+     if(is_parent(v, u)){
+        cout << u;
+        return 0; }   
+    
+    res = pre_processing(u, v);
+    
+    if(res.empty())
+        cout << "-";
+    else
+        for(auto iter: res)
+            cout << iter << ' ';
+
     return 0;
 }
